95477a822b44b5cca10e9033d51da8f5cb9cdebe
Akash Mourya <akashmourya803@gmail.com>
2025-10-26 16:42:48 +0530

    Add files via upload

---
+++ AKKA/models.py
@@ -0,0 +1,12 @@
+from extensions import db
+from datetime import datetime
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.id} from {self.name}>'
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn AKKA.app:app
+
